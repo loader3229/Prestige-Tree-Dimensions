6ff92893b6d6585b8b0ec9c60d8aae7d17b21ae1
qq1010903229 <1010903229@qq.com>
2023-06-28 11:11:42 +0800

    Update donate.js

--- js/donate.js
+++ js/donate.js
@@ -8,6 +8,7 @@ addLayer("donate", {
 	tooltip: "Donate",
 	tabFormat: [
 		"blank", "blank", "blank",
-		["raw-html", "<h1><a href=https://afdian.net/@loader3229/plan target=_blank>Afdian.net Donation</a></h1>"],
+		["raw-html", "<h1><a href=https://afdian.net/@loader3229/plan target=_blank>Afdian.net Donation (For Chinese Users)</a></h1>"],
+		["raw-html", "<h1><a href=https://ko-fi.com/loader3229 target=_blank>Buy me a coffee on Ko-Fi.com (For Non-Chinese Users)</a></h1>"],
 	],
 })
